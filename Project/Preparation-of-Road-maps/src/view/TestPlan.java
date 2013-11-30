ccf9bf6c0300d8ff83297c810fa5796a9a696840
Jonathan Ilambo <jonathan.ilambo-isombeco@insa-lyon.fr>
2013-11-30 20:18:05 +0200

    View Test panel
    Selection of intersection in the map

---
+++ Project/Preparation-of-Road-maps/src/view/TestPlan.java
@@ -0,0 +1,36 @@
+/***********************************************************************
+ * Module:  TestPlan.java
+ * Author:  Jonathan Ilambo
+ ***********************************************************************/
+
+package view;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+import model.Intersection;
+
+@SuppressWarnings("serial")
+public class TestPlan extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public TestPlan() {
+		this.setBackground(Color.red);
+
+	}
+	
+	public int EstClique(int X,int Y){
+		return -1;
+	}
+
+	public void CreerAllVueIntersecTroncon(
+			ArrayList<Intersection> getIntersections) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
